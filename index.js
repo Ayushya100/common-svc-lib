8acecdf617d5cc203ac909b584c9a23621081698
Ayushya <jaiswalayushya@gmail.com>
2025-12-22 09:29:48 +0530

    Exposed translate function from util to be used by the services

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ import dotenv from 'dotenv';
 
 import { Service } from './src/templates/index.js';
 import { infoLogger } from './src/middlewares/index.js';
-import { logger } from './src/utils/index.js';
+import { logger, translate } from './src/utils/index.js';
 
 dotenv.config({
-    path: './env'
+  path: './env'
 });
 
 export {
-    Service,
-    logger
+  Service,
+  logger,
+  translate
 };
